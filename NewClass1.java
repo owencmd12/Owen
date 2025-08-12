ea1a838fe2dc7b8e23ca8ac9266b504fe2c4d020
owencmd12 <owen.leal@cun.edu.co>
2025-08-12 12:47:20 -0500

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,10 @@
+public class NewClass1 {
+
+    public static void main(String[] args) {
+        System.out.println("@@@@@@@@@");
+        System.out.println("@| O O |@");
+        System.out.println("(|  ^  |)");
+        System.out.println(" | [_] |");
+        System.out.println(" +---+");
+    }
+}
